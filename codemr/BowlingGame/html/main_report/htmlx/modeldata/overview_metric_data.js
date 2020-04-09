d7a24f1f8ead42c30987004c4fcbea209ba081b1
shivanshseth <42113556+shivanshseth@users.noreply.github.com>
2020-04-09 12:46:32 +0530

    cleaned root directory

--- codemr/BowlingGame/html/main_report/htmlx/modeldata/overview_metric_data.js
+++
@@ -1,1 +0,0 @@
-var CodeMR_ProjectMetrics = {"Project Name":"BowlingGame", "Number of Highly Problematic Classes":0, "Number of Entities":29, "Number of Problematic Classes":1, "Class Lines of Code":1510, "Number of External Packages":18, "Number of Packages":1, "Number of External Entities":90}
